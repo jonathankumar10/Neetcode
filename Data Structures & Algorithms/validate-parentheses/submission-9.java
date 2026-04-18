7b2f1e7bc6585ead05ba2a95566422bbde7a4eed
Jonathan Sankeerth Kumar Pasupulety <49700419+jonathankumar10@users.noreply.github.com>
2026-04-18 15:46:35 -0700

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('{','}');
+        map.put('[',']');
+        map.put('(',')');
+
+        char[] sArray = s.toCharArray();
+        for(char c : sArray){
+            if(c =='{' || c == '[' || c == '('){
+                stack.push(c);
+            } else {
+                if( !stack.isEmpty() ){
+                    char value = stack.pop();
+                    if(map.get(value) == c){
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
